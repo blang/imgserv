dd1fe32575f7dbd9cabe403feb1fcf77e3108c0c
Benedikt Lang <github@benediktlang.de>
2017-01-05 23:56:49 +0100

    Save multiple files (ring buffer), serve list and last image

--- main.go
+++ main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
+	"container/ring"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	path "path/filepath"
+	"sync"
+	"time"
 )
 
-func upload(filename string) http.Handler {
+var fileBuffer = NewFileBuffer(1024)
+
+type FileBuffer struct {
+	buf *ring.Ring
+	cap int
+	sync.Mutex
+}
+
+func NewFileBuffer(size int) *FileBuffer {
+	return &FileBuffer{
+		cap: size,
+		buf: ring.New(size),
+	}
+}
+
+func (b *FileBuffer) Append(s string) {
+	b.Lock()
+	vals := b.slice()
+	if len(vals) == b.cap {
+		os.Remove(path.Join(*filepath, vals[0]))
+	}
+	b.buf.Value = s
+	b.buf = b.buf.Next()
+	b.Unlock()
+}
+
+func (b *FileBuffer) slice() []string {
+	var vals []string
+	b.buf.Do(func(f interface{}) {
+		if f != nil {
+			vals = append(vals, f.(string))
+		}
+	})
+	return vals
+}
+func (b *FileBuffer) Slice() []string {
+	b.Lock()
+	vals := b.slice()
+	b.Unlock()
+	return vals
+}
+
+func upload(filepath string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			if paused {
@@ -24,6 +70,8 @@ func upload(filename string) http.Handler {
 				return
 			}
 			defer file.Close()
+			name := fmt.Sprintf("%s.jpg", time.Now().Format("20060102-15:04:05.000"))
+			filename := path.Join(filepath, name)
 			f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
 			if err != nil {
 				log.Printf("Error uploading: %s", err)
@@ -37,7 +85,9 @@ func upload(filename string) http.Handler {
 				http.Error(w, "Error", 500)
 				return
 			}
+			fileBuffer.Append(name)
 			fmt.Fprintln(w, "Success!")
+
 		} else {
 			http.Error(w, "Not found", 404)
 		}
@@ -45,7 +95,7 @@ func upload(filename string) http.Handler {
 }
 
 const tmp = `
-<html><head><meta http-equiv="refresh" content="5; URL=/"><title>Img</title></head>
+<html><head><meta http-equiv="refresh" content="5; URL=/last"><title>Img</title></head>
 <body>
 <img src="img.jpg" style="height: 90%">
 </body></html>
@@ -53,11 +103,24 @@ const tmp = `
 
 func img(filename string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filename)
+		vals := fileBuffer.Slice()
+		if len(vals) > 0 {
+			http.ServeFile(w, r, path.Join(filename, vals[len(vals)-1]))
+		}
 	})
 }
-func index(w http.ResponseWriter, r *http.Request) {
+
+func last(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, tmp)
+}
+func index(filepath string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html><body><h1>Bilder</h1>\n")
+		for _, v := range fileBuffer.Slice() {
+			fmt.Fprintf(w, `<a href="%s">%s</a><br />`+"\n", path.Join("/imgs", v), v)
+		}
+		fmt.Fprintf(w, "</body></html>\n")
+	})
 }
 
 func auth(user, pass string, next http.Handler) http.Handler {
@@ -74,7 +137,6 @@ func auth(user, pass string, next http.Handler) http.Handler {
 
 func upauth(token string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.RequestURI)
 		if r.FormValue("token") != token {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
@@ -88,7 +150,7 @@ var (
 	password    = flag.String("password", "", "Password for basic auth")
 	uploadtoken = flag.String("uploadtoken", "", "Token for upload")
 	listen      = flag.String("listen", ":12345", "Listen")
-	filepath    = flag.String("path", "/tmp/img.jpg", "Filepath to image")
+	filepath    = flag.String("path", "/tmp/", "Filepath to image")
 )
 
 var paused = false
@@ -105,9 +167,11 @@ func pause(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
-	http.Handle("/", auth(*username, *password, http.HandlerFunc(index)))
+	http.Handle("/imgs/", auth(*username, *password, http.StripPrefix("/imgs/", http.FileServer(http.Dir(*filepath)))))
 	http.Handle("/img.jpg", auth(*username, *password, img(*filepath)))
 	http.Handle("/upload", upauth(*uploadtoken, upload(*filepath)))
 	http.Handle("/pause", auth(*username, *password, http.HandlerFunc(pause)))
+	http.Handle("/last", auth(*username, *password, http.HandlerFunc(last)))
+	http.Handle("/", auth(*username, *password, index(*filepath)))
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
